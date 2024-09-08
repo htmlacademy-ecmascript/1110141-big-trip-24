e83729c6d86f7d9d8e8a20621d77c3c425b21bb1
Kolyada Danil <kd_kein@mail.ru>
2024-09-08 22:24:13 +1000

    Добавил генерацию моковых данных для пунктов путешествия

--- src/mock/trip-event-point.js
+++ src/mock/trip-event-point.js
@@ -1,43 +1,167 @@
 import { EVENT_POINTS_TYPES } from '../const';
 import { getRandomArrayElement, getRandomInteger } from '../util';
 
-const DESTINATION_POINTS = [ 'Tokyo', 'Berlin', 'Sydney', 'Buenos Aires', 'Cairo', 'Bangkok', 'Toronto', 'Rome', 'Cape Town', 'Istanbul'];
+const DESTINATION_POINTS = [
+  {
+    id: 1,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'London',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  },
+  {
+    id: 0,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'Tokyo',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  },
+  {
+    id: 2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'Berlin',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  },
+  {
+    id: 3,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'Sydney',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  },
+  {
+    id: 4,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'Buenos Aires',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  },
+  {
+    id: 5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name:'Cairo',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomInteger}`,
+        description: 'Chamonix parliament building'
+      },
+    ]
+  }
+];
+
+const OFFERS = [
+  {
+    type: 'Taxi',
+    offers: [
+      {
+        id: 1,
+        title: 'Какой-то заголовок'
+      },
+      {
+        id: 2,
+        title: 'Какой-то заголовок №2'
+      }
+    ]
+  },
+  {
+    type: 'Bus',
+    offers: [
+      {
+        id: 1,
+        title: 'Какой-то заголовок №3'
+      },
+      {
+        id: 2,
+        title: 'Какой-то заголовок №4'
+      },
+      {
+        id: 3,
+        title: 'Какой-то заголовок №5'
+      }
+    ]
+  },
+  {
+    type: 'Train',
+    offers: [
+      {
+        id: 1,
+        title: 'Какой-то заголовок №6'
+      },
+    ]
+  }
+];
 
 const mockEventPoints = [
   {
+    id: 1,
     type: getRandomArrayElement(EVENT_POINTS_TYPES),
     destination: getRandomArrayElement(DESTINATION_POINTS),
-    startDate: new Date('2019-08-17 09:00'),
-    endDate: new Date('2019-12-25 18:00'),
-    cost: getRandomInteger(),
+    date_from: new Date('2019-08-17 09:00'),
+    date_to: new Date('2019-12-25 18:00'),
+    base_price: getRandomInteger(),
+    is_favorite: false,
+    offers: getRandomArrayElement(OFFERS).offers,
   },
   {
+    id: 2,
     type: getRandomArrayElement(EVENT_POINTS_TYPES),
     destination: getRandomArrayElement(DESTINATION_POINTS),
-    startDate: new Date('2019-06-05 09:45'),
-    endDate: new Date('2020-01-13 21:30'),
-    cost: getRandomInteger(),
+    date_from: new Date('2019-06-05 09:45'),
+    date_to: new Date('2020-01-13 21:30'),
+    base_price: getRandomInteger(),
+    is_favorite: false,
+    offers: getRandomArrayElement(OFFERS).offers,
   },
   {
+    id: 3,
     type: getRandomArrayElement(EVENT_POINTS_TYPES),
     destination: getRandomArrayElement(DESTINATION_POINTS),
-    startDate: new Date('2020-02-19 08:20'),
-    endDate: new Date('2020-03-05 02:15'),
-    cost: getRandomInteger(),
+    date_from: new Date('2020-02-19 08:20'),
+    date_to: new Date('2020-03-05 02:15'),
+    base_price: getRandomInteger(),
+    is_favorite: true,
+    offers: getRandomArrayElement(OFFERS).offers,
   },
   {
+    id: 4,
     type: getRandomArrayElement(EVENT_POINTS_TYPES),
     destination: getRandomArrayElement(DESTINATION_POINTS),
-    startDate: new Date('2020-04-11 09:00'),
-    endDate: new Date('2020-05-17 18:00'),
-    cost: getRandomInteger(),
+    date_from: new Date('2020-04-11 09:00'),
+    date_to: new Date('2020-05-17 18:00'),
+    base_price: getRandomInteger(),
+    is_favorite: false,
+    offers: getRandomArrayElement(OFFERS).offers,
   },
   {
+    id: 5,
     type: getRandomArrayElement(EVENT_POINTS_TYPES),
     destination: getRandomArrayElement(DESTINATION_POINTS),
-    startDate: new Date('2021-02-14 12:20'),
-    endDate: new Date('2021-04-10 23:30'),
-    cost: getRandomInteger(),
+    date_from: new Date('2021-02-14 12:20'),
+    date_to: new Date('2021-04-10 23:30'),
+    base_price: getRandomInteger(),
+    is_favorite: true,
+    offers: getRandomArrayElement(OFFERS).offers,
   },
 ];
 
