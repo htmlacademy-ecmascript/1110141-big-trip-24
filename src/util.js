a7dad19ece545c6c18bb5c1da5a1d2f18e55bf0a
Kolyada Danil <kd_kein@mail.ru>
2024-09-08 22:26:01 +1000

    Добавил фунцию получения случайного числа в диапазоне

--- src/util.js
+++ src/util.js
@@ -1,8 +1,18 @@
+import { MIN_PRICE, MAX_PRICE } from './const';
+
+/**
+ * Возвращает случайное целое число в диапазоне от min до max
+ * @param {Number} min Целое число нижней границы генерации
+ * @param {Number} max Целое число верхней границы генерации
+ * @returns Случайное число в диапазоне от min до max
+ */
+const getRandomInteger = (min = MIN_PRICE, max = MAX_PRICE) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 /**
  * Получает случайный элемент переданного массива
  * @param {Array} items Массив, случайный элемент которого не обходимо получить
  * @returns {any} Случайный элемент массива
  */
-const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
+const getRandomArrayElement = (items) => items[getRandomInteger(0, items.length - 1)];
 
-export {getRandomArrayElement};
+export {getRandomArrayElement, getRandomInteger};
