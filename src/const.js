6e3814ba75430800673137ccf4d03db5fb5010d6
Kolyada Danil <kd_kein@mail.ru>
2024-09-08 22:25:14 +1000

    Описал константы

--- src/const.js
+++ src/const.js
@@ -1,4 +1,6 @@
 const EVENT_POINTS_TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+const MIN_PRICE = 10;
+const MAX_PRICE = 5000;
+const DATE_FORMAT = 'd/m/y H:i';
 
-
-export {EVENT_POINTS_TYPES};
+export {EVENT_POINTS_TYPES, MAX_PRICE, MIN_PRICE, DATE_FORMAT};
