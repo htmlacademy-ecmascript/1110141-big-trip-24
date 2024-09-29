4c112f70b32b04be4b55436ffdbdce117f52223a
Kolyada Danil <kd_kein@mail.ru>
2024-09-29 18:24:36 +1000

    Сделал свойство points приватным

--- src/model/trips-model.js
+++ src/model/trips-model.js
@@ -3,9 +3,9 @@ import { getRandomEvent } from '../mock/trip-event-point';
 const TRIP_EVENT_POINTS_COUNT = 3;
 
 export default class TripsModel {
-  points = Array.from({length: TRIP_EVENT_POINTS_COUNT}, getRandomEvent);
+  #points = Array.from({length: TRIP_EVENT_POINTS_COUNT}, getRandomEvent);
 
   getEvents() {
-    return this.points;
+    return this.#points;
   }
 }
