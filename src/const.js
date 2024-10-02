b4079d14c896104c27a963641c57dc3b02835d16
Kolyada Danil <kd_kein@mail.ru>
2024-10-02 14:34:32 +1000

    Добавил перечисление фильтров

--- src/const.js
+++ src/const.js
@@ -1,5 +1,12 @@
-const EVENT_POINTS_TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+const EVENT_POINTS_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const MIN_PRICE = 10;
 const MAX_PRICE = 5000;
 
-export {EVENT_POINTS_TYPES, MAX_PRICE, MIN_PRICE};
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
+
+export {EVENT_POINTS_TYPES, MAX_PRICE, MIN_PRICE, FilterType};
