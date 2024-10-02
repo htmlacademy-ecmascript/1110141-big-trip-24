7f3844c9c4e619bc8e30becfad2cc12f592bbe31
Kolyada Danil <kd_kein@mail.ru>
2024-10-02 21:02:10 +1000

    Добавил структуру данных фильтров

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,1 +1,11 @@
-// import { FilterType } from "../const";
+import { FilterType } from '../const';
+import { isEventPast, isEventFuture, isEventPresent } from '../utils/event';
+
+const filter = {
+  [FilterType.EVERYTHING]: (events) => events,
+  [FilterType.FUTURE]: (events) => events.filter((event) => isEventFuture(event)),
+  [FilterType.PAST]: (events) => events.filter((event) => isEventPast(event)),
+  [FilterType.PRESENT]: (events) => events.filter((event) => isEventPresent(event)),
+};
+
+export { filter };
